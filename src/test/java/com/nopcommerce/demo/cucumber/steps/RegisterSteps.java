2527cc710f9b7fd5bd77343c2a87a15511bff488
prmc101 <primecommunity101@gmail.com>
2023-05-13 17:47:57 +0100

    Today's class update

---
+++ src/test/java/com/nopcommerce/demo/cucumber/steps/RegisterSteps.java
@@ -0,0 +1,32 @@
+package com.nopcommerce.demo.cucumber.steps;
+
+import cucumber.api.DataTable;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import java.util.List;
+
+/**
+ * Created by Jay Vaghani
+ */
+public class RegisterSteps {
+    @When("^I click on register link$")
+    public void iClickOnRegisterLink() {
+
+    }
+
+    @And("^I enters following users details$")
+    public void iEntersFollowingUsersDetails(DataTable dataTable) {
+        List<List<String>> userList =   dataTable.asLists(String.class);
+        for (List<String> e : userList){
+//            System.out.println(e);
+            System.out.println(e.get(1));
+        }
+    }
+
+    @Then("^registration successful$")
+    public void registrationSuccessful() {
+
+    }
+}
